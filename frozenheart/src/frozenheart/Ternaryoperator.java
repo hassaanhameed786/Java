cdea7071735c7323ae29084a7b1a4b752b0cbd78
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2019-12-27 14:09:19 +0500

    Add files via upload

---
+++ frozenheart/src/frozenheart/Ternaryoperator.java
@@ -0,0 +1,16 @@
+package frozenheart;
+
+import java.util.Scanner;
+
+public class Ternaryoperator {
+public static void main(String ags[]) {
+	
+	double gpa =2.0;
+//	System.out.println(gpa <= 2.0 ? "you are failed " : "you are passed" );
+
+	
+	
+}
+
+
+}
